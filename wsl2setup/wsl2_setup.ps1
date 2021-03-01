c83aed21fe62cee0ee83abf4eda4347871ce1293
Romira915 <40430090+Romira915@users.noreply.github.com>
2021-03-01 18:00:15 +0900

    full path get in wsl2_setup

--- wsl2setup/wsl2_setup.ps1
+++ wsl2setup/wsl2_setup.ps1
@@ -1,5 +1,6 @@
 sudo dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 sudo dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
-$commnd = 'powershell -ExecutionPolicy Unrestricted -File C:\Users\Yudai\workspace\setting\wsl2setup\wsl2_after-reboot.ps1'
+$path = Resolve-Path ..\wsl2setup\wsl2_after-reboot.ps1
+$commnd = 'powershell -ExecutionPolicy Unrestricted -File' + $path
 New-ItemProperty -LiteralPath 'HKCU:Software\Microsoft\Windows\CurrentVersion\RunOnce' -Name 'wsl2_after-reboot' -PropertyType 'String' -Value $commnd -force
 Restart-Computer
