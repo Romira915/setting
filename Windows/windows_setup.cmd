f3e12b87977c99774d0b0d5d024ab4153f488a33
Romira915 <40430090+Romira915@users.noreply.github.com>
2021-03-02 20:27:35 +0900

    winsetup::gitconfig change line

--- Windows/windows_setup.cmd
+++ Windows/windows_setup.cmd
@@ -1,5 +1,7 @@
 cd /d %~dp0
 
+powershell -ExecutionPolicy Unrestricted -File ..\scoop\scoop_setup.ps1
+
 @REM Setting git config.
 @REM Change your user name.
 git config --global user.name "Romira915" 
@@ -8,6 +10,5 @@ git config --global user.email 40430090+Romira915@users.noreply.github.com
 git config --global alias.tree "log --graph --all --format=\"%x09%C(cyan bold)%an%Creset%x09%C(yellow)%h%Creset %C(magenta reverse)%d%Creset %s\""
 git config --global init.defaultBranch main
 
-powershell -ExecutionPolicy Unrestricted -File ..\scoop\scoop_setup.ps1
 copy ..\WindowsTerminal\settings.json "%LOCALAPPDATA%\Microsoft\Windows Terminal"
 powershell -ExecutionPolicy Unrestricted -File ..\wsl2setup\wsl2_setup.ps1
