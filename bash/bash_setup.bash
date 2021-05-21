9d4dc1f379f1b8b003b4e3c1437ce19d854aa88a
Romira915 <40430090+Romira915@users.noreply.github.com>
2021-05-21 15:36:43 +0900

    gitbash change for JP

--- bash/bash_setup.bash
+++ bash/bash_setup.bash
@@ -1,5 +1,15 @@
 cd `dirname $0`
 
+echo "export LC_ALL=ja_JP.utf8" >> ~/.bashrc
+echo "export LANG=ja_JP.utf8" >> ~/.bashrc
+echo "export LANGUAGE=ja_JP.utf8" >> ~/.bashrc
+echo "export LC_CTYPE=\"ja_JP.utf8\"" >> ~/.bashrc
+echo "export LC_NUMERIC=\"ja_JP.utf8\"" >> ~/.bashrc
+echo "export LC_TIME=\"ja_JP.utf8\"" >> ~/.bashrc
+echo "export LC_COLLATE=\"ja_JP.utf8\"" >> ~/.bashrc
+echo "export LC_MONETARY=\"ja_JP.utf8\"" >> ~/.bashrc
+echo "export LC_MESSAGES=\"ja_JP.utf8\"" >> ~/.bashrc
+
 git clone https://github.com/chris-marsh/pureline.git ~/pureline
 cp ./.pureline.conf ~/.pureline.conf
 
