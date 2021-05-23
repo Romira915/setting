ce287f5e95188431d7d8a452661e52ebfbcbd67f
Romira915 <40430090+Romira915@users.noreply.github.com>
2021-05-23 20:32:34 +0900

    vscode interactive

--- winget/winget_setup.sh
+++ winget/winget_setup.sh
@@ -1,3 +1,3 @@
-winget install -e -id Microsoft.VisualStudioCode
+winget install -e -id -i Microsoft.VisualStudioCode
 winget install -e -id BlueStack.BlueStacks
 winget install -e -id Microsoft.VisualStudio.Enterprise
