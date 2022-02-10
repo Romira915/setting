0dedd5cc6c06ef6d62f3aeeddd8e0f924aaaeae6
Romira915 <40430090+Romira915@users.noreply.github.com>
2022-02-10 19:59:44 +0900

    fix font setup

--- Windows11/fonts_setup.ps1
+++ Windows11/fonts_setup.ps1
@@ -1,6 +1,6 @@
 Set-Location (Split-Path $script:myInvocation.MyCommand.path -parent)
 
-$env:USERPROFILE\scoop\shims\git.exe clone https://github.com/mzyy94/RictyDiminished-for-Powerline.git
+& $env:USERPROFILE\scoop\shims\git.exe clone https://github.com/mzyy94/RictyDiminished-for-Powerline.git
 Invoke-WebRequest -UseBasicParsing -Uri https://github.com/microsoft/cascadia-code/releases/download/v2102.25/CascadiaCode-2102.25.zip -OutFile CascadiaCode-2102.25.zip
 
 
