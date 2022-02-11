f875ac9bdc46a23b7d5febe780df8796353c6a51
Romira915 <40430090+Romira915@users.noreply.github.com>
2022-02-11 21:19:49 +0900

    add wsl for win11

--- wsl2/wsl2_for_windows11.ps1
+++ wsl2/wsl2_for_windows11.ps1
@@ -1,5 +1,3 @@
 Set-Location (Split-Path $script:myInvocation.MyCommand.path -parent)
-Invoke-WebRequest -Uri https://aka.ms/wslubuntu2004 -OutFile Ubuntu2004.appx -UseBasicParsing
-Add-AppxPackage -Path Ubuntu2004.appx
-rm Ubuntu2004.appx
+wsl --install -d Ubuntu
 pause
