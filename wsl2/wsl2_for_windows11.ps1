2738bedf11b695d9d5cb7bb03fccf46a536456cb
Romira915 <40430090+Romira915@users.noreply.github.com>
2022-02-12 18:49:07 +0900

    add wsl after reboot

--- wsl2/wsl2_for_windows11.ps1
+++ wsl2/wsl2_for_windows11.ps1
@@ -1,3 +1,9 @@
 Set-Location (Split-Path $script:myInvocation.MyCommand.path -parent)
 wsl --install
+
+$path = Resolve-Path .\wsl2_for_windows11_after_reboot.ps1
+$commnd = 'powershell -ExecutionPolicy Unrestricted -File ' + $path
+New-ItemProperty -LiteralPath 'HKCU:Software\Microsoft\Windows\CurrentVersion\RunOnce' -Name 'wsl2_for_windows11_after_reboot' -PropertyType 'String' -Value $commnd -force
+
 pause
+Restart-Computer
---
+++ wsl2/wsl2_for_windows11_after_reboot.ps1
@@ -0,0 +1,2 @@
+Set-Location (Split-Path $script:myInvocation.MyCommand.path -parent)
+wsl --install -d Ubuntu
