b8a40886074a4161030e37845ded66ecd8dc1ded
Romira915 <40430090+Romira915@users.noreply.github.com>
2022-04-04 15:57:40 +0900

    add font

--- Windows11/fonts_setup.ps1
+++ Windows11/fonts_setup.ps1
@@ -2,6 +2,7 @@ Set-Location (Split-Path $script:myInvocation.MyCommand.path -parent)
 
 & $env:USERPROFILE\scoop\shims\git.exe clone https://github.com/mzyy94/RictyDiminished-for-Powerline.git
 Invoke-WebRequest -UseBasicParsing -Uri https://github.com/microsoft/cascadia-code/releases/download/v2102.25/CascadiaCode-2102.25.zip -OutFile CascadiaCode-2102.25.zip
+Invoke-WebRequest -UseBasicParsing -Uri https://github.com/yuru7/PlemolJP/releases/download/v1.2.3/PlemolJP_NF_v1.2.3.zip -OutFile PlemolJP_NF_v1.2.3.zip
 
 
 # Fonts install
