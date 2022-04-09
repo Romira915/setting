18b1504a9f7f857554d910c33ab65ae3ab92c1bc
Romira915 <40430090+Romira915@users.noreply.github.com>
2022-04-09 15:22:43 +0900

    fix: move vsocde

--- winget/winget_setup.ps1
+++ winget/winget_setup.ps1
@@ -1,2 +1,3 @@
+winget install -e --id Microsoft.VisualStudioCode --override "/mergetasks=addcontextmenufiles,addcontextmenufolders,!runcode /lang=english /verysilent"
 winget install -e --id BlueStack.BlueStacks
 winget install -e --id Microsoft.VisualStudio.Enterprise
