bacb7f784ddcfe2abe9931dbd4d40f337965c5f7
Romira915 <40430090+Romira915@users.noreply.github.com>
2022-10-24 00:19:25 +0900

    add: ln gitconfig

---
+++ Mac/mac_setup.sh
@@ -0,0 +1,7 @@
+ln -fs $HOME/.config/romira-s-config/git/.gitconfig ~/.gitconfig
+
+zsh ../zsh/preztoinit.sh
+ln -fs $HOME/.config/romira-s-config/zsh/.zpreztorc ~/.zpreztorc
+
+brew install --cask notion
+brew install --cask google-japanese-ime
