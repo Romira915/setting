2f205b93867db6a0517ecbf24336f5db44b02b82
Romira915 <40430090+Romira915@users.noreply.github.com>
2024-10-28 16:42:57 +0900

    add: gcsdk go

--- fish/config.fish
+++ fish/config.fish
@@ -32,3 +32,6 @@ function get_gh_repo
 end
 
 source ~/.config/mcfly/mcfly.fish
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/yuudai.tanaka/.tmp/google-cloud-sdk/path.fish.inc' ]; . '/Users/yuudai.tanaka/.tmp/google-cloud-sdk/path.fish.inc'; end
